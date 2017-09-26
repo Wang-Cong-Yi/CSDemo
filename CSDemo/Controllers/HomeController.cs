3df3dad3f85febb2fb2b4ed3034e36bb125f2e71
王从艺 <@DESKTOP-65DES31>
2017-09-26 17:20:58 +0800

    添加项目文件。

---
+++ CSDemo/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //第一次上传
+            return View();
+        }
+    }
+}
